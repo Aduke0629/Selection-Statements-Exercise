8d9b43075678f6fb0ee286c4631e2e3ba833ad17
Aaron Duke <aaron.duke0817@gmail.com>
2023-02-20 13:43:59 -0500

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 175;
+
+            Console.WriteLine("Try to guess my favorite number");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == favNumber)
+            {
+                Console.WriteLine($"Correct");
+            }
+
+            else if(userInput < favNumber)
+            {
+                Console.WriteLine($"Too Low");
+            }
+
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine($"Too High");
+            }
+
+            else
+            {
+                Console.WriteLine($"Nevermind");
+            }
+
+
+
+            
+
+           
+
+      
         }
     }
 }
